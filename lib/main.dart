62490fa76a91be28dfae9fec646632c4ea501db8
Mohammad Alamoudi <mhmd.amoudi@bootfi.com>
2025-03-11 15:09:01 +0300

    add examples in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,51 @@
 import 'package:flutter/material.dart';
 
+import 'core/core.dart';
+
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final _controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: ListView(
+          children: [
+            Padding(
+              padding: CustomEdgeInsets.except(8, bottom: 24),
+              child: Text('This is Custom Padding'),
+            ),
+            TextField(controller: _controller),
+            SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () async {
+                final text =
+                    _controller.text.trim().isNotEmpty
+                        ? _controller.text.trim()
+                        : null;
+                print(text);
+              },
+              child: Text('Check'),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
