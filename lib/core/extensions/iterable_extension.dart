c9e85797c0c18db0b7187564324d9ee633a37597
Mohammad Alamoudi <134515468+mu7mmd@users.noreply.github.com>
2025-07-19 00:07:08 +0300

    Update iterable_extension.dart

--- lib/core/extensions/iterable_extension.dart
+++ lib/core/extensions/iterable_extension.dart
@@ -1,8 +1,24 @@
-extension IterableExtension<E> on List<E> {
+extension ListExtension<E> on List<E> {
   E? firstWhereOrNull(bool Function(E element) test) {
     for (final element in this) {
       if (test(element)) return element;
     }
     return null;
+  }
+
+  E firstWhereOrFirst(bool Function(E element) test) {
+    for (final element in this) {
+      if (test(element)) return element;
+    }
+
+    return first;
+  }
+
+  E firstWhereOrLast(bool Function(E element) test) {
+    for (final element in this) {
+      if (test(element)) return element;
+    }
+
+    return last;
   }
 }
